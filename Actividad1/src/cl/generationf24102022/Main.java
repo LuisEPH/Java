56391bfbf3d1d737e8e433a2b0c1daec5753696e
LuisEPH <luispinoh@outlook.com>
2022-10-24 12:28:01 -0300

    Actividad 1 Java

---
+++ Actividad1/src/cl/generationf24102022/Main.java
@@ -0,0 +1,45 @@
+package cl.generationf24102022;
+
+public class Main {
+
+    public static void main(String[] args) {
+                  
+    	int num = 56;  
+    	float num2 = num;  //1. int a float
+    	System.out.println(num2); 
+
+    	int num3 = 100;
+    	float num4 = (float) num3;//2. int a float modo 2
+    	System.out.println(num4);
+    	//////////////////
+    	float a=8.61f;
+    	int b;//3. Float a int
+    	b=(int)a;
+    	System.out.println(b);
+
+    	/////////////////Kiara
+
+
+                  int numero1 = 20;//4. int a float
+                  float num1afloat1 = numero1;
+                  System.out.println(num1afloat1);
+                  
+                  //int p=78;
+                  long num1along1 = numero1;//5. Int a long
+                  System.out.println(num1along1);
+                  
+                  double num1adouble1 = numero1;//6. Int a double
+                  System.out.println(num1adouble1);
+                  
+                  long numero2 = 234567;
+                  int numero2aint1 = (int)numero2;
+                  System.out.println(numero2aint1);//7. Long a int
+                  float num2afloat1 = numero2;
+
+                  /////////////////////Cynthia
+                  int numm = 10;
+                  double otronum = 30;
+                  double result = numm*otronum;//8. Multiplicación int a doublé.
+                  System.out.println(result);
+    	}
+	}
